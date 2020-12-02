f542bbea4daf623241e01683ae0b2703fcea3827
AbdelrhmanAmin <maxhd213@gmail.com>
2020-12-02 22:43:06 +0200

    Fix linters 1.0

--- bin/main.rb
+++ bin/main.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
 $winning = false
-$board = [" "," ", " ", " ", " ", " ", " ", " ", " "]
+$board = [' ',' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
 class Game
   attr_reader :name
   def initialize(name, symbol)
@@ -14,10 +14,10 @@ class Game
     if pos.to_i < 10 
       pos = pos.to_i - 1
       $board.each_with_index do |num, i|
-        if i == pos && num == " "
+        if i == pos && num == ' '
           $board[i] = @symbol
-        elsif i == pos && num != " "
-          puts "POSITION UNAVAILABLE!! TRY AGAIN!!"
+        elsif i == pos && num != ' '
+          puts 'POSITION UNAVAILABLE!! TRY AGAIN!!'
           play()
         end
   
@@ -40,93 +40,92 @@ class Game
 
 end
 
-player1 = Game.new("Neko Master", "X")
-player2 = Game.new("Ana Paula", "O")
+player1 = Game.new('Neko Master', 'X')
+player2 = Game.new('Ana Paula', 'O')
 
 while $winning == false 
   player1.play
   player1.display
-  if $board[0] == $board[1] && $board[1] == $board[2] && $board[0] != " "
+  if $board[0] == $board[1] && $board[1] == $board[2] && $board[0] != ' '
     $winning = true
     puts "Congratulations #{player1.name}, you won!"
     break
-  elsif $board[3] == $board[4] && $board[4] == $board[5] && $board[3] != " "
+  elsif $board[3] == $board[4] && $board[4] == $board[5] && $board[3] != ' '
     break
     $winning = true
     puts "Congratulations #{player1.name}, you won!"
-  elsif $board[6] == $board[7] && $board[7] == $board[8] && $board[6] != " "
+  elsif $board[6] == $board[7] && $board[7] == $board[8] && $board[6] != ' '
     $winning = true
     puts "Congratulations #{player1.name}, you won!"
     break
-  elsif $board[0] == $board[3] && $board[3] == $board[6] && $board[0] != " "
+  elsif $board[0] == $board[3] && $board[3] == $board[6] && $board[0] != ' '
     $winning = true
     puts "Congratulations #{player1.name}, you won!"
     break
-  elsif $board[1] == $board[4] && $board[4] == $board[7] && $board[1] != " "
+  elsif $board[1] == $board[4] && $board[4] == $board[7] && $board[1] != ' '
     $winning = true
     puts "Congratulations #{player1.name}, you won!"
     break
-  elsif $board[2] == $board[5] && $board[5] == $board[8] && $board[2] != " "
+  elsif $board[2] == $board[5] && $board[5] == $board[8] && $board[2] != ' '
     $winning = true
     puts "Congratulations #{player1.name}, you won!"
     break
-  elsif $board[0] == $board[4] && $board[4] == $board[8] && $board[0] != " "
+  elsif $board[0] == $board[4] && $board[4] == $board[8] && $board[0] != ' '
     $winning = true
     puts "Congratulations #{player1.name}, you won!"
     break
-  elsif $board[2] == $board[4] && $board[4] == $board[6] && $board[2] != " "
+  elsif $board[2] == $board[4] && $board[4] == $board[6] && $board[2] != ' '
     $winning = true
     puts "Congratulations #{player1.name}, you won!"
     break
   end
-  if $board.include?(" ") == false
-    puts "GAME OVER!!!"
+  if $board.include?(' ') == false
+    puts 'GAME OVER!!!'
     break
   end
 
   player2.play
   player2.display
 
-  if $board[0] == $board[1] && $board[1] == $board[2] && $board[0] != " "
+  if $board[0] == $board[1] && $board[1] == $board[2] && $board[0] != ' '
     $winning = true
     puts "Congratulations #{player2.name}, you won!"
     break
-  elsif $board[3] == $board[4] && $board[4] == $board[5] && $board[3] != " "
+  elsif $board[3] == $board[4] && $board[4] == $board[5] && $board[3] != ' '
     break
     $winning = true
     puts "Congratulations #{player2.name}, you won!"
-  elsif $board[6] == $board[7] && $board[7] == $board[8] && $board[6] != " "
+  elsif $board[6] == $board[7] && $board[7] == $board[8] && $board[6] != ' '
     $winning = true
     puts "Congratulations #{player2.name}, you won!"
     break
-  elsif $board[0] == $board[3] && $board[3] == $board[6] && $board[0] != " "
+  elsif $board[0] == $board[3] && $board[3] == $board[6] && $board[0] != ' '
     $winning = true
     puts "Congratulations #{player2.name}, you won!"
     break
-  elsif $board[1] == $board[4] && $board[4] == $board[7] && $board[1] != " "
+  elsif $board[1] == $board[4] && $board[4] == $board[7] && $board[1] != ' '
     $winning = true
     puts "Congratulations #{player2.name}, you won!"
     break
-  elsif $board[2] == $board[5] && $board[5] == $board[8] && $board[2] != " "
+  elsif $board[2] == $board[5] && $board[5] == $board[8] && $board[2] != ' '
     $winning = true
     puts "Congratulations #{player2.name}, you won!"
     break
-  elsif $board[0] == $board[4] && $board[4] == $board[8] && $board[0] != " "
+  elsif $board[0] == $board[4] && $board[4] == $board[8] && $board[0] != ' '
     $winning = true
     puts "Congratulations #{player2.name}, you won!"
     break
-  elsif $board[2] == $board[4] && $board[4] == $board[6] && $board[2] != " "
+  elsif $board[2] == $board[4] && $board[4] == $board[6] && $board[2] != ' '
     $winning = true
     puts "Congratulations #{player2.name}, you won!"
     break
   end
-  if $board.include?(" ") == false
-    puts "GAME OVER!!!"
+  if $board.include?(' ') == false
+    puts 'GAME OVER!!!'
     break
   end
 
 end
-
 
 
 
