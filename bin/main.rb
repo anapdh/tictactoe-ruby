100e46ca5df440613390c4da9c1100ae229a58ec
AbdelrhmanAmin <maxhd213@gmail.com>
2020-12-02 19:37:52 +0200

    Add display board

--- bin/main.rb
+++ bin/main.rb
@@ -6,49 +6,41 @@
 #Player2 will have the remainig symbol.
 
 # HOW TO SELECT PLAYER
-
-
-
+winning = false
 class Game
   
-  @symbol = [0, 1, 2]
-  @@board = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+  @@board = [" "," ", " ", " ", " ", " ", " ", " ", " "]
 
   def initialize(name, symbol)
     @name = name
     @symbol = symbol
   end
-
   def play()
     puts "CHOOSE YOUR POSITION!"
     pos = gets.chomp 
     pos = pos.to_i - 1
     @@board.each_with_index do |num, i|
-      if i == pos && num.to_i == 0
+      if i == pos && num == " "
         @@board[i] = @symbol
-        print @@board
       end
+  
     end
   end
-
   def display
-    puts 
-    "#{@@board[0]} | #{@@board[1]} | #{@@board[2]}
-    ----------
-    #{@@board[3]} | #{@@board[4]} | #{@@board[5]}
-    ----------
-    #{@@board[6]} | #{@@board[7]} | #{@@board[8]}"
+  
+    puts"    #{@@board[0]} | #{@@board[1]} | #{@@board[2]}
+      ----------
+      #{@@board[3]} | #{@@board[4]} | #{@@board[5]}
+      ----------
+      #{@@board[6]} | #{@@board[7]} | #{@@board[8]}"
   end
 
 end
 
 player1 = Game.new("p1", "X")
-# player1.play
 player2 = Game.new("p2", "O")
-player2.play
-
-#Player 1 selects 1 of the 9 spaces to fill.
-
+player1.play
+player1.display
 
 
 #continues untill one of the players fill 3 spaces in sequence.
@@ -59,3 +51,14 @@ player2.play
 # DIAGONAL2 if position[2] == position[4] && position[4] == position[6]
 
 #play Winning message for the first who get there.
+
+# while if winning = false
+#   player1.play
+#   player1.display
+
+
+  # if board == full
+  # puts "GAME IS OVER!! DRAW!!"
+
+
+
