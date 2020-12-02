2f931526be92bbff947211955d6c66a7c5ae3e58
AbdelrhmanAmin <maxhd213@gmail.com>
2020-12-02 22:02:01 +0200

    Fix cornercases

--- bin/main.rb
+++ bin/main.rb
@@ -1,14 +1,7 @@
 #!/usr/bin/env ruby
 
-
-
-#Player1 need to select X or O.
-#Player2 will have the remainig symbol.
-
-# HOW TO SELECT PLAYER
 $winning = false
 $board = [" "," ", " ", " ", " ", " ", " ", " ", " "]
-lastPlayer = "P:"
 class Game
   attr_reader :name
   def initialize(name, symbol)
@@ -17,18 +10,24 @@ class Game
   end
   def play()
     puts " #{self.name} is playing \n CHOOSE YOUR POSITION!"
-    lastPlayer = self.name
     pos = gets.chomp 
-    pos = pos.to_i - 1
-    $board.each_with_index do |num, i|
-      if i == pos && num == " "
-        $board[i] = @symbol
-      elsif i == pos && num != " "
-        puts "POSITION UNAVAILABLE!! TRY AGAIN!!"
-        play()
-      end
+    if pos.to_i < 10 
+      pos = pos.to_i - 1
+      $board.each_with_index do |num, i|
+        if i == pos && num == " "
+          $board[i] = @symbol
+        elsif i == pos && num != " "
+          puts "POSITION UNAVAILABLE!! TRY AGAIN!!"
+          play()
+        end
   
+    
+      end
+    else
+      puts "#{self.name.upcase} DON'T CHEAT!!!!!!! inserted an invalid number! \n ------------------"
+      play()
     end
+    
   end
   def display
   
@@ -45,50 +44,36 @@ player1 = Game.new("Neko Master", "X")
 player2 = Game.new("Ana Paula", "O")
 
 while $winning == false 
-  
-  
   player1.play
   player1.display
-  # rows
-  ## first row
   if $board[0] == $board[1] && $board[1] == $board[2] && $board[0] != " "
     $winning = true
     puts "Congratulations #{player1.name}, you won!"
     break
-  ## second row
   elsif $board[3] == $board[4] && $board[4] == $board[5] && $board[3] != " "
     break
     $winning = true
     puts "Congratulations #{player1.name}, you won!"
-  ## third row
   elsif $board[6] == $board[7] && $board[7] == $board[8] && $board[6] != " "
     $winning = true
     puts "Congratulations #{player1.name}, you won!"
     break
-
-    #columns
-    ## first column
   elsif $board[0] == $board[3] && $board[3] == $board[6] && $board[0] != " "
     $winning = true
     puts "Congratulations #{player1.name}, you won!"
     break
-    ## second -column
   elsif $board[1] == $board[4] && $board[4] == $board[7] && $board[1] != " "
     $winning = true
     puts "Congratulations #{player1.name}, you won!"
     break
-    ## third column
   elsif $board[2] == $board[5] && $board[5] == $board[8] && $board[2] != " "
     $winning = true
     puts "Congratulations #{player1.name}, you won!"
     break
-
-    ## first diagonal
   elsif $board[0] == $board[4] && $board[4] == $board[8] && $board[0] != " "
     $winning = true
     puts "Congratulations #{player1.name}, you won!"
     break
-    ## second diagonal
   elsif $board[2] == $board[4] && $board[4] == $board[6] && $board[2] != " "
     $winning = true
     puts "Congratulations #{player1.name}, you won!"
@@ -106,40 +91,30 @@ while $winning == false
     $winning = true
     puts "Congratulations #{player2.name}, you won!"
     break
-  ## second row
   elsif $board[3] == $board[4] && $board[4] == $board[5] && $board[3] != " "
     break
     $winning = true
     puts "Congratulations #{player2.name}, you won!"
-  ## third row
   elsif $board[6] == $board[7] && $board[7] == $board[8] && $board[6] != " "
     $winning = true
     puts "Congratulations #{player2.name}, you won!"
     break
-
-    #columns
-    ## first column
   elsif $board[0] == $board[3] && $board[3] == $board[6] && $board[0] != " "
     $winning = true
     puts "Congratulations #{player2.name}, you won!"
     break
-    ## second -column
   elsif $board[1] == $board[4] && $board[4] == $board[7] && $board[1] != " "
     $winning = true
     puts "Congratulations #{player2.name}, you won!"
     break
-    ## third column
   elsif $board[2] == $board[5] && $board[5] == $board[8] && $board[2] != " "
     $winning = true
     puts "Congratulations #{player2.name}, you won!"
     break
-
-    ## first diagonal
   elsif $board[0] == $board[4] && $board[4] == $board[8] && $board[0] != " "
     $winning = true
     puts "Congratulations #{player2.name}, you won!"
     break
-    ## second diagonal
   elsif $board[2] == $board[4] && $board[4] == $board[6] && $board[2] != " "
     $winning = true
     puts "Congratulations #{player2.name}, you won!"
