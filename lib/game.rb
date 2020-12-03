b7fdc0c1461ceef46d8300b7d7d079a7255d1dc3
AbdelrhmanAmin <maxhd213@gmail.com>
2020-12-03 21:35:31 +0200

    Add refactoring

---
+++ lib/game.rb
@@ -0,0 +1,13 @@
+# Class for game logic
+
+def game(player1,player2,boardC, board)
+  game_on = true
+  while game_on
+  player1.play(board)
+  boardC.display(board)
+  player2.play(board)
+  boardC.display(board)
+  
+end
+end
+
--- lib/player.rb
+++ lib/player.rb
@@ -6,4 +6,24 @@ class Player
     @name = name
     @symbol = symbol
   end
+
+  def play(arr)
+    puts " #{name} is playing \n CHOOSE YOUR POSITION!"
+    pos = gets.chomp.to_i
+    if pos < 10
+      pos -= 1
+      arr.each_with_index do |num, i|
+        if i == pos && num == ' '
+          arr[i] = @symbol
+        elsif i == pos && num != ' '
+          puts 'POSITION UNAVAILABLE!! TRY AGAIN!!'
+          play(arr)
+        end
+      end
+    else
+      puts 'Invalid number!! Try Again!'
+      play(arr)
+    end
+  end
+
 end
