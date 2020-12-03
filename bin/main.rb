d8d4d8c2a8e9755e01b76678f3e189af2e4b1cce
anapdh <ana_d_hubner@hotmail.com>
2020-12-03 18:34:06 -0300

    fix linters 1.1

--- bin/main.rb
+++ bin/main.rb
@@ -12,11 +12,11 @@ game_rules = Rules.new
 puts 'Would you like to read the game rules/instructions? (yes, no)'
 answer = gets.chomp
 
-  if answer == 'yes'
-    puts game_rules.display_rules
-  else
-    puts "\n Ok, let's start the game! \n "
-  end
+if answer == 'yes'
+  puts game_rules.display_rules
+else
+  puts "\n Ok, let's start the game! \n "
+end
 
 puts "Please, enter name of Player 'X':"
 player1 = Player.new(gets.chomp, 'X')
