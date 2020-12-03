53725f993f0b157e7de79b5b5178b5eb50d7e31b
AbdelrhmanAmin <maxhd213@gmail.com>
2020-12-03 20:06:36 +0200

    Encapsulate code

--- bin/main.rb
+++ bin/main.rb
@@ -3,7 +3,7 @@
 
 require_relative '../lib/player'
 require_relative '../lib/board'
-
+require_relative '../lib/game.rb'
 board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
 puts "Please, enter name of Player 'X':"
 player1 = Player.new(gets.chomp, 'X')
@@ -11,31 +11,5 @@ puts "Please, enter name of Player 'O':"
 player2 = Player.new(gets.chomp, 'O')
 new_board_game = Board.new(board)
 puts new_board_game.display(board)
-
-public
-
-def play(arr)
-  puts " #{name} is playing \n CHOOSE YOUR POSITION!"
-  pos = gets.chomp.to_i
-  if pos < 10
-    pos -= 1
-    arr.each_with_index do |num, i|
-      if i == pos && num == ' '
-        arr[i] = @symbol
-      elsif i == pos && num != ' '
-        puts 'POSITION UNAVAILABLE!! TRY AGAIN!!'
-        play(arr)
-      end
-    end
-  else
-    puts 'Invalid number!! Try Again!'
-    play(arr)
-  end
-end
-
-player1.play(board)
-new_board_game.display(board)
-player2.play(board)
-new_board_game.display(board)
-
+game(player1,player2,new_board_game,board)
 # rubocop:enable Metrics/PerceivedComplexity
