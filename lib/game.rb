18f7d5736c29248ee89e186ece2610a41735a675
AbdelrhmanAmin <maxhd213@gmail.com>
2020-12-03 21:38:49 +0200

    Fixing styling

--- lib/game.rb
+++ lib/game.rb
@@ -3,11 +3,10 @@
 def game(player1,player2,boardC, board)
   game_on = true
   while game_on
-  player1.play(board)
-  boardC.display(board)
-  player2.play(board)
-  boardC.display(board)
-  
-end
+    player1.play(board)
+    boardC.display(board)
+    player2.play(board)
+    boardC.display(board)
+  end
 end
 
