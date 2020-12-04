e4e6841f2e4e99981aee004f44b5c98f4775a820
AbdelrhmanAmin <maxhd213@gmail.com>
2020-12-04 21:48:16 +0200

    Refactoring code

--- bin/main.rb
+++ bin/main.rb
@@ -1,28 +1,64 @@
 #!/usr/bin/env ruby
 
 require_relative '../lib/player'
-require_relative '../lib/board'
 require_relative '../lib/rules'
+require_relative '../lib/game'
 
-game_on = true
+def display(board)
+  puts "
+      #{board[6] == ' ' ? 7 : board[6]} | #{board[7] == ' ' ? 8 : board[7]} | #{board[8] == ' ' ? 9 : board[8]}
+      -----------
+      #{board[3] == ' ' ? 4 : board[3]} | #{board[4] == ' ' ? 5 : board[4]} | #{board[5] == ' ' ? 6 : board[5]}
+      -----------
+      #{board[0] == ' ' ? 1 : board[0]} | #{board[1] == ' ' ? 2 : board[1]} | #{board[2] == ' ' ? 3 : board[2]}"
+end
 board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
 
-game_rules = Rules.new
+game_on = true
+turn = true
 
+
+game_rules = Rules.new
 puts 'Would you like to read the game rules/instructions? (yes, no)'
 answer = gets.chomp
-
 if answer == 'yes'
   puts game_rules.display_rules
 else
   puts "\n Ok, let's start the game! \n "
 end
-
 puts "Please, enter name of Player 'X':"
-player1 = Player.new(gets.chomp, 'X')
+player1 = gets.chomp
 puts "Please, enter name of Player 'O':"
-player2 = Player.new(gets.chomp, 'O')
-new_board_game = Board.new(board)
-puts new_board_game.display(board)
+player2 = gets.chomp
+game = Game.new(player1,player2)
+display(board)
 
-move(gets.chomp, board)
+while game_on
+  pos = gets.chomp
+  if game.valid?(pos)
+
+
+    if game.free?(pos, board) == true
+      puts "TRUE"
+      game.move(pos,board, turn)
+    elsif game.free?(pos, board) == false
+      puts "POSITION #{pos} TAKEN! Please, Try again."
+      display(board)
+      next
+    end
+
+  else
+    puts "INVALID NUMBER! Please, Try again."
+    game.move(pos,board, turn)
+  end
+  display(board)
+  turn = !turn
+  if game.did_win?(board)
+    puts turn ? "#{player1} Won" : "#{player2} Won"
+    break
+  end
+  if game.did_draw?(board)
+    puts "GAME OVER"
+    break
+  end
+end
--- lib/player.rb
+++ lib/player.rb
@@ -2,62 +2,9 @@
 # rubocop:disable Metrics/PerceivedComplexity
 class Player
   attr_accessor :name, :symbol
-
   def initialize(name, symbol)
     @name = name
     @symbol = symbol
   end
-
-  # def play(arr)
-  #   #puts " #{name} is playing... \n CHOOSE YOUR POSITION! \n "
-  #   # pos = gets.chomp.to_i
-  #   if pos < 10 && pos > 0 && pos.is_a?(Integer)
-  #     pos -= 1
-  #     arr.each_with_index do |num, i|
-  #       if i == pos && num == ' '
-  #         arr[i] = @symbol
-  #       elsif i == pos && num != ' '
-  #         #puts " \n POSITION UNAVAILABLE!! Please, try again. \n "
-  #         play(arr)
-  #       end
-  #     end
-  #   else
-  #     #puts " \n INVALID NUMBER!! Please, try again. \n "
-  #     play(arr)
-  #   end
-  # end
-
-  # 1st method:
-  # move method - get the position player want to play.
-
-    def move(pos)
-      arr.each_with_index do |num, i|
-        if i == pos && num == ' '
-        arr[i] = symbol
-        end
-      end
-    end
-
-  # 2nd method:
-  # checks if position is available
-
-  def checks(pos)
-    arr.each_with_index do |num, i|
-      if i == pos && symbol 
-        false
-  end
-
-
-  # 3rd method:
-  # checks if number given is invalis or not
-
-  def invalid(pos)
-    arr.each_with_index do |num, i|
-      if pos < 10 && pos.positive? && pos.is_a?(Integer)
-        false
-      end
-    end   
-  end
-
 end
 # rubocop:enable Metrics/PerceivedComplexity
