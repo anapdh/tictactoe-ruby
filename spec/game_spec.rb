076ad0fe872395ac0d016598743ebb19802dc7cb
anapdh <ana_d_hubner@hotmail.com>
2020-12-14 13:34:52 -0300

    add initialize game

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,16 +1,24 @@
-require_relative './lib/game.rb'
+require_relative '../lib/game.rb'
 
 describe Game do
 
   let(:player1) { 'Name1' }
   let(:player2) { 'Name2' }
+  # let(:symbol)
   let(:game) { Game.new(player1, player2) }
 
 
   context 'initialize' do
 
-    it 'Start the player 1 and player 2' do
-    expect(player.new).to eql ("Name1" "X")
-    expect(player.new).to eql ("Name2" "0")
+    it 'Start the player 1' do
+      expect(player1).to eql ('Name1')
+      expect(game.player1.symbol).to eql ('X')
+    end
+
+    it 'Start the player 2' do
+      expect(player2).to eql ('Name2')
+      expect(game.player2.symbol).to eql ('O')
+    end
+
   end
 end
